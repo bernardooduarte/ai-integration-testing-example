6a236a8f710de7213f8a1bd94a574eabecee5dbb
bernardo.o.dev <bernardo.o.dev@NXA3XxmYBX0My43.radixengrj.matriz>
2026-02-13 16:23:46 -0300

    fazendo correções

--- spring-api/src/main/java/com/bernardo_duarte/spring_api/SpringApiApplication.java
+++ spring-api/src/main/java/com/bernardo_duarte/spring_api/SpringApiApplication.java
@@ -2,12 +2,19 @@ package com.bernardo_duarte.spring_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class SpringApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApiApplication.class, args);
+	}
+
+	@Bean
+	public RestClient.Builder restClientBuilder(){
+		return RestClient.builder();
 	}
 
 }
